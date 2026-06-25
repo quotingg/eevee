74141dfd2854922d725be782f3842f742c7ba6e3
quotingg. <112746878+quotingg@users.noreply.github.com>
2026-06-24 21:37:12 -0400

    Add remote control attribute to account attributes

--- Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
@@ -538,6 +538,10 @@ private func modifyAttributes(_ attributes: inout [String: AccountAttribute]) {
         $0.stringValue = "99999999999999999"
     }
 
+    attributes["remote-control"] = AccountAttribute.with {
+        $0.stringValue = "1"
+    }
+
     attributes.removeValue(forKey: "payment-state")
     attributes.removeValue(forKey: "last-premium-activation-date")
     
