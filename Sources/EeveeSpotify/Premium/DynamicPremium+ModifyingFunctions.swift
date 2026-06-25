8e7c496ecf607f218ac0c07523ca66fbb8ee7824
quotingg. <112746878+quotingg@users.noreply.github.com>
2026-06-24 22:28:13 -0400

    Change boolean value assignment to string in attributes

--- Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
@@ -425,7 +425,7 @@ private func modifyAttributes(_ attributes: inout [String: AccountAttribute]) {
     }
 
     attributes["streaming-only-premium"] = AccountAttribute.with {
-        $0.boolValue = true
+        $0.boolValue = "1"
     }
 
     attributes["offline"] = AccountAttribute.with {
