7970c09d92b6ff8cc57411f296a4210aa9eb3111
quotingg. <112746878+quotingg@users.noreply.github.com>
2026-06-30 23:53:49 -0400

    Update EeveePremiumForce.x.swift

--- Sources/EeveeSpotify/EeveePremiumForce.x.swift
+++ Sources/EeveeSpotify/EeveePremiumForce.x.swift
@@ -17,10 +17,11 @@ private func forcedPremiumString(forKey key: String) -> String? {
     case "player-license-v2":                      return "premium"
     case "financial-product":                      return "pr:premium,tc:0"
 
-    case "aa-state"                                return "COMPLETED"
-    case "dsa-mode-available"                      return "1"
-    case "dsa-mode-enabled"                        return "1"
-
+    case "aa-state":                               return "COMPLETED"
+    case "dsa-mode-available":                     return "1"
+    case "dsa-mode-enabled":                       return "1"
+    case "audiobook-onboarding-completed":         return "1"
+        
     //spotify src | capabilites: {canDownload: dn("1" === e.offline, "1" === e["streaming-only-premium"])}
     //spotify lets you download stuff if you also have streaming-only-premium enabled
     case "streaming-only-premium":                 return "1"
