be50f596b81b9cd5f622f9ab8ee9de9c92a4e72f
quotingg. <112746878+quotingg@users.noreply.github.com>
2026-07-01 00:11:36 -0400

    Update DynamicPremium+ModifyingFunctions.swift

--- Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+ModifyingFunctions.swift
@@ -384,13 +384,13 @@ private func modifyAttributes(_ attributes: inout [String: AccountAttribute]) {
         $0.stringValue = "COMPLETED"
     }
 
-    attributes["dsa-mode-available"] = AccountAttribute.with {
+    /*attributes["dsa-mode-available"] = AccountAttribute.with {
         $0.boolValue = true
     }
 
     attributes["dsa-mode-enabled"] = AccountAttribute.with {
         $0.boolValue = true
-    }
+    }*/
     
     attributes["ab-ad-player-targeting"] = AccountAttribute.with {
         $0.boolValue = false
